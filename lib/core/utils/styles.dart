fbb1cd9aa6e749da31164ba6fb1bf599dcec32a2
yousefmohamed35 <yousefmo2021@gmail.com>
2024-10-13 21:37:45 +0300

    finish styles and app component

---
+++ lib/core/utils/styles.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+abstract class Styles {
+  static const textStyle48 = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 48.0,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Gilroy',
+  );
+  static const textStyle18 = TextStyle(
+    color: Color(0xffFFFFFF),
+    fontSize: 18,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Gilroy',
+  );
+  static const textStyle26 = TextStyle(
+    color: Colors.black,
+    fontSize: 26,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Gilroy',
+  );
+  static const textStyle16 = TextStyle(
+    color: Color(0xff7C7C7C),
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Gilroy',
+  );
+  static const textStyle14 = TextStyle(
+    color: Color(0xff181725),
+    fontSize: 14,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Gilroy',
+  );
+  static const textStyle24 = TextStyle(
+    color: Color(0xff181725),
+    fontSize: 24,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Gilroy',
+  );
+  static const textStyle13 = TextStyle(
+    color: Color(0xff7C7C7C),
+    fontSize: 13,
+    fontWeight: FontWeight.w400,
+    fontFamily: 'Gilroy',
+  );
+}
