47eb497f1d8e58f8c8b1dd4384bd9ac2261d25a0
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-10-15 20:32:12 +0300

    finish login view ui

---
+++ lib/core/utils/widgets/custom_text_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:nectarappmvvm/core/utils/styles.dart';
+
+class CustomTextButton extends StatelessWidget {
+  const CustomTextButton({
+    super.key,
+    required this.text,
+    this.color = const Color(0xff181725),
+  });
+  final String text;
+  final Color? color;
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        style: ButtonStyle(
+            padding: WidgetStatePropertyAll(
+          EdgeInsets.zero,
+        )),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: Styles.textStyle14.copyWith(color: color),
+        ));
+  }
+}
--- lib/features/Auth/persentation/views/login_view.dart
+++ lib/features/Auth/persentation/views/login_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:nectarappmvvm/constants.dart';
 import 'package:nectarappmvvm/core/utils/images.dart';
 import 'package:nectarappmvvm/core/utils/styles.dart';
+import 'package:nectarappmvvm/core/utils/widgets/custom_button.dart';
+import 'package:nectarappmvvm/core/utils/widgets/custom_text_button.dart';
 import 'package:nectarappmvvm/core/utils/widgets/password_text_form_field.dart';
 
 import '../../../../core/utils/widgets/email_text_form_field.dart';
@@ -12,60 +14,95 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(Images.loginBackgroundImage),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-            Center(child: Image.asset(Images.carrotImage)),
-            SizedBox(
-              height: 80,
-            ),
-            Text(
-              "Login",
-              style: Styles.textStyle26,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "Enter your emails and password",
-              style: Styles.textStyle16,
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Email",
-              style: Styles.textStyle16,
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            EmailTextFormField(),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Password",
-              style: Styles.textStyle16,
-            ),
-            SizedBox(
-              height: 8,
-            ),
-            PasswordTextFormField(),
-          ],
-        ),
-      ),
+      body: LoginViewBody(),
     );
   }
 }
 
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(Images.loginBackgroundImage),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: ListView(
+        children: [
+          SizedBox(
+            height: 80,
+          ),
+          Center(child: Image.asset(Images.carrotImage),),
+          SizedBox(
+            height: 80,
+          ),
+          Text(
+            "Login",
+            style: Styles.textStyle26,
+          ),
+          Text(
+            "Enter your emails and password",
+            style: Styles.textStyle16,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Email",
+            style: Styles.textStyle16,
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          EmailTextFormField(),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            "Password",
+            style: Styles.textStyle16,
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          PasswordTextFormField(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              CustomTextButton(
+                text: 'Forget Password?',
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          CustomButton(text: 'Log In'),
+          SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                r"Don't have an account?",
+                style: Styles.textStyle14,
+              ),
+              CustomTextButton(
+                text: 'SignUP',
+                color: Constants.kPrimaryColor,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
